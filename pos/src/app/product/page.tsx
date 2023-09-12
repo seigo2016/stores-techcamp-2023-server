be5b73591fc7d0ffa3e7126e2642c9357c339b8e
hayato.shishido <hayato.shishido@hayatoshishidonoMacBook-Pro.local>
2023-09-12 17:33:19 +0900

    Remove unused codes

--- pos/src/app/product/page.tsx
+++ pos/src/app/product/page.tsx
@@ -7,21 +7,6 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { getProducts } from "@/usecases";
 
-const products: Product[] = [
-  {
-    id: 1,
-    name: "キリッとBLACK ブラックコーヒー（無糖）235ml",
-    price: 248,
-    url: "https://sarutahiko.jp/cdn/shop/products/641d07a182919f421274c08f555cbb0e.jpg?v=1677639512",
-  },
-  {
-    id: 2,
-    name: "職人のカフェラテ（甘くない）235ml",
-    price: 270,
-    url: "https://sarutahiko.jp/cdn/shop/products/ee80406ce792f4332098e3c960873e24.jpg?v=1677639185",
-  },
-];
-
 export default function PageProduct() {
   const router = useRouter();
 
