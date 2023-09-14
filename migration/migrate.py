75c885bd2333d47255b1cb37fc6b59d82afdb730
seigo2016 <mail@seigo2016.com>
2023-09-14 14:22:19 +0900

    fix migration data

--- migration/migrate.py
+++ migration/migrate.py
@@ -6,14 +6,14 @@ def mutation():
     bodies.append("""
         mutation {
   item: addItem(input: [
-    { xid: "sample1" name:"キリッとBLACK ブラックコーヒー（無糖）235ml" price: 248 preview: "https://sarutahiko.jp/cdn/shop/products/641d07a182919f421274c08f555cbb0e.jpg?v=1677639512"}
-    { xid: "sample2" name:"職人のカフェラテ（甘くない）235ml" price: 270 preview: "https://sarutahiko.jp/cdn/shop/products/ee80406ce792f4332098e3c960873e24.jpg?v=1677639185"}
-    { xid: "item1" name: "item1"  price: 500}
-    { xid: "item2" name: "item2"  price: 500}
-    { xid: "item3" name: "item3"  price: 500}
-    { xid: "item4" name: "item4"  price: 500}
-    { xid: "item5" name: "item5"  price: 500}
-    { xid: "item6" name: "item6"  price: 500}
+    { xid: "sample1" name:"キリッとBLACK ブラックコーヒー（無糖）235ml" price: 248 preview: "https://sarutahiko.jp/cdn/shop/products/641d07a182919f421274c08f555cbb0e.jpg?v=1677639512" }
+    { xid: "sample2" name:"職人のカフェラテ（甘くない）235ml" price: 270 preview: "https://sarutahiko.jp/cdn/shop/products/ee80406ce792f4332098e3c960873e24.jpg?v=1677639185" }
+    { xid: "sample3" name: "無糖/リキッドアイスコーヒー(瓶) 600ml" price: 972 preview: "https://sarutahiko.jp/cdn/shop/files/ada02dffe8beee60fdb185acef5cf00f.jpg?v=1694488768&width=1206" }
+    { xid: "sample4" name: "無糖/リキッドアイスコーヒー（大吉ブレンド）" price: 980 preview: "https://sarutahiko.jp/cdn/shop/files/72cfd54c8b5c9252e1465ea5fbc33b47.jpg?v=1682392413&width=1206" }
+    { xid: "sample5" name: "オキーニョ" price: 825 preview: "https://sarutahiko.jp/cdn/shop/products/4257d69fd793772bb43abed4514a2a17.jpg?v=1677575294&width=1206" }
+    { xid: "sample6" name: "バゲット" price: 308 preview: "https://sarutahiko.jp/cdn/shop/products/b2cd87424f8277e58f12a77ec166ba4a.jpg?v=1677575483&width=1206" }
+    { xid: "sample7" name: "コールドブリュー珈琲ゼリー" price: 388 preview: "https://sarutahiko.jp/cdn/shop/files/bdab1acaa21dadc45ef5aae445dbe662.jpg?v=1686909393&width=1206" }
+    { xid: "sample8" name: "アーモンドフロランタン" price: 578 preview: "https://sarutahiko.jp/cdn/shop/products/6b837e5fd7869813909c2b4c8c3cd02f.jpg?v=1677462786&width=1206" }     
   ]) { numUids }
 }
         """)
@@ -32,9 +32,9 @@ def mutation():
         item: updateUser(input: {
         filter: { xid: { eq: "user1" } }
         set: { boughtItems: [
-          { xid: "item1" }
-          { xid: "item2" }
-          { xid: "item3" }
+          { xid: "sample1" }
+          { xid: "sample2" }
+          { xid: "sample3" }
         ] }
       }) { numUids }}
     """)
@@ -45,9 +45,9 @@ def mutation():
         item: updateUser(input: {
         filter: { xid: { eq: "user2" } }
         set: { boughtItems: [
-          { xid: "item1" }
-          { xid: "item3" }
-          { xid: "item6" }
+          { xid: "sample1" }
+          { xid: "sample3" }
+          { xid: "sample6" }
         ] }
       }) { numUids }}
     """)
@@ -57,10 +57,10 @@ def mutation():
         item: updateUser(input: {
         filter: { xid: { eq: "user3" } }
         set: { boughtItems: [
-          { xid: "item1" }
-          { xid: "item2" }
-          { xid: "item3" }
-          { xid: "item4" }
+          { xid: "sample1" }
+          { xid: "sample2" }
+          { xid: "sample3" }
+          { xid: "sample4" }
         ] }
       }) { numUids }}
     """)
@@ -68,7 +68,7 @@ def mutation():
     bodies.append("""
       mutation {
         item: updateItem(input: {
-        filter: { xid: { eq: "item1" } }
+        filter: { xid: { eq: "sample1" } }
         set: { users: [
           { xid: "user1"}
           { xid: "user2" }
@@ -80,7 +80,7 @@ def mutation():
     bodies.append("""
       mutation {
         item: updateItem(input: {
-        filter: { xid: { eq: "item2" } }
+        filter: { xid: { eq: "sample2" } }
         set: { users: [
           { xid: "user1"}
           { xid: "user3" }
@@ -91,7 +91,7 @@ def mutation():
     bodies.append("""
       mutation {
         item: updateItem(input: {
-        filter: { xid: { eq: "item3" } }
+        filter: { xid: { eq: "sample3" } }
         set: { users: [
           { xid: "user1" }
           { xid: "user2" }
@@ -103,7 +103,7 @@ def mutation():
     bodies.append("""
       mutation {
         item: updateItem(input: {
-        filter: { xid: { eq: "item4" } }
+        filter: { xid: { eq: "sample4" } }
         set: { users: [
           { xid: "user3" }
         ] }
@@ -113,7 +113,7 @@ def mutation():
     bodies.append("""
       mutation {
         item: updateItem(input: {
-        filter: { xid: { eq: "item6" } }
+        filter: { xid: { eq: "sample6" } }
         set: { users: [
           { xid: "user2" }
         ] }
